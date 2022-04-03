69d224c19b66b9d051f585cc78f02547126e3808
Yulv-git <yulvchi@qq.com>
2022-04-03 20:31:20 +0800

    Add init weights and example imgs download scripts.

---
+++ src/SonoNet/example_images/download_example.sh
@@ -0,0 +1,24 @@
+###
+ # @Author: Shuangchi He / Yulv
+ # @Email: yulvchi@qq.com
+ # @Date: 2022-04-03 20:19:22
+ # @Motto: Entities should not be multiplied unnecessarily.
+ # @LastEditors: Shuangchi He
+ # @LastEditTime: 2022-04-03 20:29:18
+ # @FilePath: /Awesome-Ultrasound-Standard-Plane-Detection/src/SonoNet/example_images/download_example.sh
+ # @Description: Modify here please
+### 
+
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/3VV.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/4CH.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/ABDOMINAL.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/BRAIN-CB.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/BRAIN-TV.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/FEMUR.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/KIDNEYS.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/LIPS.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/LVOT.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/PROFILE.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/RVOT.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/SPINE-CORONAL.tiff
+wget https://github.com/rdroste/SonoNet_PyTorch/raw/master/example_images/SPINE-SAGITTAL.tiff
