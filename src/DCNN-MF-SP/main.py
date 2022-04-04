1e0a116fe5fc0fe4be41088d4e6d76b56920f33a
Yulv-git <yulvchi@qq.com>
2022-04-04 21:47:43 +0800

    Add base model.

--- src/DCNN-MF-SP/main.py
+++ src/DCNN-MF-SP/main.py
@@ -6,7 +6,7 @@ Email: yulvchi@qq.com
 Date: 2022-04-03 18:28:14
 Motto: Entities should not be multiplied unnecessarily.
 LastEditors: Shuangchi He
-LastEditTime: 2022-04-03 23:05:17
+LastEditTime: 2022-04-04 21:40:06
 FilePath: /Awesome-Ultrasound-Standard-Plane-Detection/src/DCNN-MF-SP/main.py
 Description: Evaluation of Deep Convolutional Neural Networks for Automatic Classification of Common Maternal Fetal Ultrasound Planes
 Init from https://github.com/Oussamayousre/automatic-classification-of-common-maternal-fetal-ultrasound-planes b784f0107fd8cd0368622c5da09a0b41d0a3eb04
@@ -28,6 +28,7 @@ import seaborn as sns
 import pydicom as dicom
 import tensorflow as tf
 import wandb
+from ipdb import set_trace
 from sklearn.model_selection import train_test_split
 from keras_preprocessing.image.dataframe_iterator import DataFrameIterator
 from keras_preprocessing.image import ImageDataGenerator
@@ -36,64 +37,17 @@ from keras.utils import data_utils
 from keras.applications.vgg19 import VGG19
 from tensorflow.keras.applications.resnet50 import ResNet50
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers.convolutional import Conv2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, BatchNormalization
 from keras.layers.core import Activation
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from keras.callbacks import ReduceLROnPlateau
-from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D,GlobalAveragePooling2D
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, GlobalAveragePooling2D
 from glob import glob
 from keras import backend as K
 from tensorflow.keras.models import load_model
 from wandb.keras import WandbCallback
 
-
-class ImageFrameGenerator(tf.keras.utils.Sequence):  # Generates data for Keras.
-    def __init__(self, train_ID, batch_size=1, image_list_path=image_train_array, label_list=label_train_array,
-                 image_batch=32, dataset=df_test, dim=(256,256), shuffle=True):
-        self.batch_size = batch_size
-        self.image_list_path = image_list_path
-        self.label_list = label_list
-        self.image_batch = image_batch
-        self.train_ID = train_ID   
-        self.dataset = dataset
-        self.shuffle = shuffle
-        self.dim = dim
-        self.on_epoch_end()
-
-    def __len__(self):  # Denotes the number of batches per epoch.
-        return int(np.floor(len(self.train_ID)))
-
-    def __getitem__(self, index):  # Generate one batch of data.
-        # Generate indexes of the batch.
-        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
-        # Find list of IDs.
-        list_IDs_temp = [k for k in indexes]
-        # list_IDs_temp = [20]
-        # Generate data.
-        data, labels = self.__data_generation(list_IDs_temp)
-
-        return data/255, labels
-
-    def on_epoch_end(self):  # Updates indexes after each epoch.
-        self.indexes = np.arange(len(self.train_ID))
-        if self.shuffle == True:
-            np.random.shuffle(self.indexes)
-
-    def __data_generation(self, list_IDs_temp):  # Generates data containing batch_size samples.  # X: (n_samples, *dim, n_channels)
-        data = np.array([]).reshape(0,256,256,3)
-        labels = np.array([])
-        first_index = list_IDs_temp[0] * self.image_batch
-        for i in np.arange(32):
-            # img = tf.keras.preprocessing.image.array_to_img(data)
-            image_name = f"./Images/{self.image_list_path[first_index+i]}.png" 
-            image = cv2.imread(image_name)
-            image = cv2.resize(image,(256,256))
-            image = image.reshape((1,256,256,3))
-            data = np.vstack([data,image])
-            labels = np.append(labels,self.label_list[first_index+i])
-
-        return data, labels
+from src.utils import check_dir, recall_m, precision_m, f1_m, load_train_generator
+from src.base_model_net import Network as base_network
 
 
 def main(args):
@@ -103,36 +57,39 @@ def main(args):
     wandb_callback = wandb.keras.WandbCallback(log_weights=args.log_weights)  # Pre-processing and EDA.
 
     # data
-    list_images = os.listdir(args.imgs_dir)  # len(list_images)  # 12400
-    df = pd.read_excel(args.imgs_info, index_col=0, engine='openpyxl')  # 12400 rows × 6 columns
-    df["Image_name"] = df.index  # Change the Dataframe index.  # 12400 rows × 7 columns
-    df.reset_index(drop=True, inplace=True)  # Reset the Dataframe indexes.
-    df.rename(columns={'Train ': 'Train'}, inplace=True)
-    df.Train[df.Train == 0]  # Length: 5271, dtype: int64
-    df.Image_name = "{}/{}.png".format(args.imgs_dir, df.Image_name)
-    df.Plane.unique()  # array(['Other', 'Maternal cervix', 'Fetal abdomen', 'Fetal brain', 'Fetal femur', 'Fetal thorax'], dtype=object)
-    # df['Plane'] = df['Plane'].map({'Other':0, 'Maternal cervix':1, 'Fetal abdomen':2, 'Fetal brain':3, 'Fetal femur':4, 'Fetal thorax':5})
-    df = df.sample(frac=1).reset_index(drop=True)  # Shuffle the Dataframe rows.
+    train_generator, valid_generator = load_train_generator(
+        imgs_info=args.imgs_info, imgs_dir=args.imgs_dir, x_col="Image_name", y_cols="Plane",
+        shuffle=True, batch_size=args.batch_size, seed=1, target_w=args.img_size[0], target_h=args.img_size[1])
 
-    # Split the Data.
-    df_test = df[df["Train"] == 0]  # Testing data.
-    df_train = df[df["Train"] == 1]  # Training Data.
-    # Reset Indexes after Spliting the Data.
-    df_train.reset_index(drop=True, inplace=True)
-    df_test.reset_index(drop=True, inplace=True)
-    image_train_array = df_train.Image_name.to_numpy()  # 7129
-    label_train_array = df_train.Plane.to_numpy()
-    image_test_array = df_test.Image_name.to_numpy()
-    label_test_array = df_test.Plane.to_numpy()
+    # Baseline Model
+    base_model = base_network(input_channels=args.input_channels, img_size=args.img_size, cls_num=args.cls_num)
+    # Plot the model Architecture.
+    tf.keras.utils.plot_model(base_model, to_file='{}/base_model.png'.format(args.save_dir))
+    
+    # Model Training
+    learning_rate_reduction = ReduceLROnPlateau(monitor='accuracy', patience=2, verbose=1, factor=0.1, min_lr=0.000001)
+    opt = tf.keras.optimizers.Adam(learning_rate=args.learning_rate)
+    base_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy', f1_m, precision_m, recall_m])
+    history = base_model.fit(train_generator, validation_data=valid_generator, epochs=args.epochs, callbacks=[learning_rate_reduction, wandb_callback])
 
-    # Image Data generation using Sequence.
-    train_params = {'dim':(256, 256), 'batch_size':1, 'shuffle':False, 'dataset':df_train,
-                    'image_list_path':image_train_array, 'label_list':label_train_array}
-    test_params = {'dim':(256,256), 'batch_size':1, 'shuffle':False, 'dataset':df_test,
-                   'image_list_path':image_test_array, 'label_list':label_test_array}
-    training_generator = ImageFrameGenerator(np.arange(df_train.Train.size//32 - 1), **train_params)
-    testing_generator = ImageFrameGenerator(np.arange(df_test.Train.size//32 - 1), **test_params)
-    datagen=ImageDataGenerator(rescale=1./255., validation_split=0.25)
+    # Plot loss during training.
+    plt.subplot(211)
+    plt.title('Loss')
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='test')
+    plt.legend()
+    # Plot accuracy during training.
+    plt.subplot(212)
+    plt.title('Accuracy')
+    plt.plot(history.history['accuracy'], label='train')
+    plt.plot(history.history['val_accuracy'], label='test')
+    plt.legend()
+    plt.savefig('{}/base_model_training_acc_loss.png'.format(args.save_dir))
+    
+    # Save the model.
+    base_model.save('{}/base_model'.format(args.save_dir), save_format='tf')
+    # Load the model.
+    model = tf.keras.models.load_model('{}/base_model'.format(args.save_dir), compile=False)
 
 
 if __name__ == '__main__':
@@ -146,6 +103,17 @@ if __name__ == '__main__':
     # data
     parse.add_argument('--imgs_dir', type=str, default='../../data/FETAL_PLANES_DB/Images')
     parse.add_argument('--imgs_info', type=str, default='../../data/FETAL_PLANES_DB/FETAL_PLANES_DB_data.xlsx')
-    
+    parse.add_argument('--batch_size', type=int, default=64)
+    parse.add_argument('--img_size', default=(256, 256), help='(target_w, target_h)')
+    parse.add_argument('--cls_num', default=6)
+
+    #
+    parse.add_argument('--input_channels', default=3)
+    parse.add_argument('--learning_rate', default=0.0001)
+    parse.add_argument('--epochs', default=15)
+    parse.add_argument('--save_dir', default='./output')
+
     args = parse.parse_args()
+    check_dir(args.save_dir)
+    
     main(args)
