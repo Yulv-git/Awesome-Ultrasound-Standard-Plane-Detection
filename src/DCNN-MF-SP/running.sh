274b7c5f85945b07a17c55e9474262d69216e043
Yulv-git <yulvchi@qq.com>
2022-04-05 21:47:57 +0800

    Add shell script.

--- src/DCNN-MF-SP/running.sh
+++ src/DCNN-MF-SP/running.sh
@@ -1,24 +1,14 @@
-<!--
- * @Author: Shuangchi He / Yulv
- * @Email: yulvchi@qq.com
- * @Date: 2022-04-03 18:16:07
- * @Motto: Entities should not be multiplied unnecessarily.
- * @LastEditors: Shuangchi He
- * @LastEditTime: 2022-04-05 21:09:44
- * @FilePath: /Awesome-Ultrasound-Standard-Plane-Detection/src/DCNN-MF-SP/Readme.md
- * @Description: Evaluation of Deep Convolutional Neural Networks for Automatic Classification of Common Maternal Fetal Ultrasound Planes
- * Init from https://github.com/Oussamayousre/automatic-classification-of-common-maternal-fetal-ultrasound-planes b784f0107fd8cd0368622c5da09a0b41d0a3eb04
--->
+###
+ # @Author: Shuangchi He / Yulv
+ # @Email: yulvchi@qq.com
+ # @Date: 2022-04-05 20:56:19
+ # @Motto: Entities should not be multiplied unnecessarily.
+ # @LastEditors: Shuangchi He
+ # @LastEditTime: 2022-04-05 21:13:34
+ # @FilePath: /Awesome-Ultrasound-Standard-Plane-Detection/src/DCNN-MF-SP/running.sh
+ # @Description:
+### 
 
-<h1><center> Evaluation of Deep Convolutional Neural Networks for Automatic Classification of Common Maternal Fetal Ultrasound Planes </center></h1>
-
-# Dataset
-
-[Fetal_Planes_DB](../../data/FETAL_PLANES_DB/)
-
-# Running
-
-``` bash
 CUDA_VISIBLE_DEVICES=0 python main.py --model_type base_model --model_name base_model
 
 CUDA_VISIBLE_DEVICES=1 python main.py --model_type VGG --model_name VGG19
@@ -34,4 +24,3 @@ CUDA_VISIBLE_DEVICES=4 python main.py --model_type EfficientNet --model_name Eff
 # CUDA_VISIBLE_DEVICES=4 python main.py --model_type EfficientNet --model_name EfficientNetB6 --imagenet_pretrained
 
 CUDA_VISIBLE_DEVICES=5 python main.py --model_type ViT --model_name ViT
-```
