342db4e7fc17ea8174fa249de44ac4ddb0105e0e
Yulv-git <yulvchi@qq.com>
2022-04-09 00:06:23 +0800

    Update shell scripts.

--- src/DCNN-MF-SP/running.sh
+++ src/DCNN-MF-SP/running.sh
@@ -4,7 +4,7 @@
  # @Date: 2022-04-05 20:56:19
  # @Motto: Entities should not be multiplied unnecessarily.
  # @LastEditors: Shuangchi He
- # @LastEditTime: 2022-04-08 22:41:04
+ # @LastEditTime: 2022-04-08 23:48:29
  # @FilePath: /Awesome-Ultrasound-Standard-Plane-Detection/src/DCNN-MF-SP/running.sh
  # @Description:
 ### 
@@ -25,11 +25,11 @@ CUDA_VISIBLE_DEVICES=1 python main.py --model_type DCNN --model_name ResNet50
 # CUDA_VISIBLE_DEVICES=1 python main.py --model_type DCNN --model_name ResNet50 --imagenet_pretrained True
 
 CUDA_VISIBLE_DEVICES=1 python main.py --model_type DCNN --model_name DenseNet121 --batch_size 48
-# CUDA_VISIBLE_DEVICES=1 python main.py --model_type DCNN --model_name DenseNet121 --batch_size 48 --imagenet_pretrained
+# CUDA_VISIBLE_DEVICES=1 python main.py --model_type DCNN --model_name DenseNet121 --batch_size 48 --imagenet_pretrained True
 
 
 CUDA_VISIBLE_DEVICES=1 python main.py --model_type EfficientNet --model_name EfficientNetB6 --batch_size 8
-# CUDA_VISIBLE_DEVICES=1 python main.py --model_type EfficientNet --model_name EfficientNetB6 --batch_size 8 --imagenet_pretrained
+# CUDA_VISIBLE_DEVICES=1 python main.py --model_type EfficientNet --model_name EfficientNetB6 --batch_size 8 --imagenet_pretrained True
 
 
 CUDA_VISIBLE_DEVICES=1 python main.py --model_type ViT --model_name ViT --batch_size 4
